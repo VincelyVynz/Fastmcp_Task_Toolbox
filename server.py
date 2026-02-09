2aecd3605615ad9d4b8cbde26c6b714335aecc6f
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2026-02-09 17:30:23 +0530

    Add percentage calculator to mcp server

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from fastmcp import FastMCP
+from tools.math_tools import calculate_percentage
 
 mcp = FastMCP("Task Toolbox")
 
+@mcp.tool()
+def get_percentage(part: float, whole: float)-> str:
+    """Calculates what percentage 'part' is of 'whole'."""
+    return calculate_percentage(part, whole)
 
 if __name__ == "__main__":
     mcp.run()
